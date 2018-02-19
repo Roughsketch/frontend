243cd0c58c5cd2f77592b43614116688823d4944
Maid Dog <maiddogsrl@gmail.com>
2018-02-19 08:49:49 -0600

    Start using files

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,29 @@
 
 extern crate rocket;
 
+use std::io;
+use std::path::{Path, PathBuf};
+
+use rocket::response::{content, NamedFile};
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello"
+fn index() -> io::Result<NamedFile> {
+    NamedFile::open("static/index.html")
+}
+
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+#[error(404)]
+fn not_found() -> io::Result<NamedFile> {
+    NamedFile::open("static/missing.html")
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, files])
+        .catch(errors![not_found])
+        .launch();
 }
